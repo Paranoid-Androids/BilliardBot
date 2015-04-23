50b6a506867894b9660488229b7f8f7a43b90049
Oleg Vaskevich <me@olegvaskevich.com>
2015-04-22 23:02:02 -0400

    Improve test framework

--- tests/testutils.js
+++ tests/testutils.js
@@ -3,11 +3,25 @@ define(function(require) {
 
     var TestUtils = {};
 
+    var Matter = require('third_party/matter');
+    var GameLogic = require('gamelogic');
+
     TestUtils.startTestGame = function(gameType) {
         var canvas = document.getElementById('matter-canvas');
         var common = require('common');
         common.startGame(canvas, gameType);
-    }
+    };
+
+    TestUtils.addBallToWorld = function(gui, color, ballNo, x, y) {
+        var ball = gui.createBall({
+            x: x,
+            y: y
+        });
+
+        ball.render.fillStyle = color;
+        ball.label = GameLogic.BALL_LABEL_PREFIX + ballNo;
+        Matter.World.add(gui.engine.world, ball);
+    };
 
     return TestUtils;
 });
